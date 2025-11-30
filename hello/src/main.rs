a7f0bd48ecdcbb30d353558c6646fdcad27d7baf
Christopher Stingl <cstingl@protonmail.com>
2025-11-29 19:09:08 -0500

    Changing over to a testable `world()` function

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,10 @@
-use hello::print;
+use hello::world;
 
 fn main() {
-    print();
+    println!("{}", world());
+}
+
+#[test]
+fn world_returns_hello_world() {
+    assert_eq!(world(), "Hello, world!", "wrong message")
 }
