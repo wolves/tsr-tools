2ef9f4265b950a4fb5578c67a1303378f20d9783
Christopher Stingl <cstingl@protonmail.com>
2025-11-29 19:09:55 -0500

    Move test to lib instead of main

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn world() -> String {
     String::from("Hello, world!")
 }
+
+#[test]
+fn world_returns_hello_world() {
+    assert_eq!(world(), "Hello, world!", "wrong message")
+}
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,8 +3,3 @@ use hello::world;
 fn main() {
     println!("{}", world());
 }
-
-#[test]
-fn world_returns_hello_world() {
-    assert_eq!(world(), "Hello, world!", "wrong message")
-}
