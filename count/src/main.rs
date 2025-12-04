e6a092960282e88e90f915ca0c0d34255f2f2c4c
Wolves <cstingl@protonmail.com>
2025-12-04 17:18:36 -0500

    Adding way to address bailing out early if no args present

--- count/src/main.rs
+++ count/src/main.rs
@@ -1,10 +1,15 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use std::{env, fs::File, io::BufReader};
 
 use count::count_lines;
 
 fn main() -> Result<()> {
-    for path in env::args().skip(1) {
+    let args: Vec<_> = env::args().skip(1).collect();
+    if args.is_empty() {
+        bail!("Usage: count <FILE>...");
+    }
+
+    for path in args {
         let file =
             File::open(&path).with_context(|| path.clone())?;
         let file = BufReader::new(file);
