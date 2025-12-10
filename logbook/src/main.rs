3cb7cfe6340cfa6c90947792a6865f3869783b14
Christopher Stingl <cstingl@protonmail.com>
2025-12-09 20:27:21 -0500

    Minimal protoype logbook cli

--- logbook/src/main.rs
+++ logbook/src/main.rs
@@ -1,12 +1,27 @@
-use std::{fs::File, io::Write};
-
 use anyhow::Result;
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+};
 
 fn main() -> Result<()> {
-    let mut logbook = File::options()
-        .create(true)
-        .append(true)
-        .open("logbook.txt")?;
-    writeln!(logbook, "Hello, logbook!")?;
+    let args: Vec<_> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        if fs::exists("logbook.txt")? {
+            let text = fs::read_to_string("logbook.txt")?;
+            print!("{text}");
+        } else {
+            println!("Logbook is empty");
+        }
+    } else {
+        let mut logbook = File::options()
+            .create(true)
+            .append(true)
+            .open("logbook.txt")?;
+        writeln!(logbook, "{}", args.join(" "))?;
+    }
+
     Ok(())
 }
