99e0f39f3da3f795450d3836df6cf98e44ef3576
Christopher Stingl <cstingl@protonmail.com>
2025-12-09 19:53:59 -0500

    Initial logbook spike code

---
+++ logbook/src/main.rs
@@ -0,0 +1,12 @@
+use std::{fs::File, io::Write};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let mut logbook = File::options()
+        .create(true)
+        .append(true)
+        .open("logbook.txt")?;
+    writeln!(logbook, "Hello, logbook!")?;
+    Ok(())
+}
