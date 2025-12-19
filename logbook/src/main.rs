8efa2f592748c4a81a5eea27dbfccc88538c15ce
Wolves <cstingl@protonmail.com>
2025-12-19 09:36:41 -0500

    Updating main fn to make use of fn in lib

--- logbook/src/main.rs
+++ logbook/src/main.rs
@@ -1,26 +1,18 @@
+use std::env;
+
 use anyhow::Result;
-use std::{
-    env,
-    fs::{self, File},
-    io::Write,
-};
 
 fn main() -> Result<()> {
     let args: Vec<_> = env::args().skip(1).collect();
 
     if args.is_empty() {
-        if fs::exists("logbook.txt")? {
-            let text = fs::read_to_string("logbook.txt")?;
+        if let Some(text) = logbook::read("logbook.txt")? {
             print!("{text}");
         } else {
             println!("Logbook is empty");
         }
     } else {
-        let mut logbook = File::options()
-            .create(true)
-            .append(true)
-            .open("logbook.txt")?;
-        writeln!(logbook, "{}", args.join(" "))?;
+        logbook::append("logbook.txt", &args.join(" "))?;
     }
 
     Ok(())
