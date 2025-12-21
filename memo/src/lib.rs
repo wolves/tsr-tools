ca02b8d27d6769ec642e46a35a8cee170f6ab563
Christopher Stingl <cstingl@protonmail.com>
2025-12-21 11:05:57 -0500

    Building out open lib function and tests

--- memo/src/lib.rs
+++ memo/src/lib.rs
@@ -1,9 +1,16 @@
-use std::path::Path;
-
-use anyhow::Result;
+use std::{
+    fs::{self, File},
+    io::{BufRead, BufReader, Result},
+    path::Path,
+};
 
 pub fn open(path: impl AsRef<Path>) -> Result<Vec<String>> {
-    Ok(vec![])
+    if fs::exists(&path)? {
+        let file = BufReader::new(File::open(&path)?);
+        file.lines().collect()
+    } else {
+        Ok(Vec::new())
+    }
 }
 
 #[cfg(test)]
@@ -14,5 +21,11 @@ mod tests {
     fn open_returns_data_from_given_file() {
         let memos: Vec<_> = open("tests/data/memos.txt").unwrap();
         assert_eq!(memos, vec!["foo", "bar"], "wrong data");
+    }
+
+    #[test]
+    fn open_returns_empty_vec_for_missing_file() {
+        let memos = open("bogus.txt").unwrap();
+        assert!(memos.is_empty(), "vec not empty");
     }
 }
