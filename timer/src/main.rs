0899603b10130a14866bc9e73b7c315a2b3cba56
Wolves <cstingl@protonmail.com>
2025-12-31 09:18:18 -0500

    Initial rough prototype

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,10 +1,26 @@
+use std::{env, process::Command, time::Instant};
+
 use anyhow::Result;
-use std::process::Command;
+use clap::{CommandFactory, Parser};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, required = true)]
+    cmd: String,
+}
 
 fn main() -> Result<()> {
-    let mut cmd = Command::new("cargo");
+    if env::args().count() < 2 {
+        Args::command().print_long_help();
+        return Ok(());
+    }
+    let args = Args::parse();
+
+    let start = Instant::now();
+    let mut cmd = Command::new(args.cmd);
     let out = cmd.output()?;
     let stdout = String::from_utf8_lossy(&out.stdout);
     println!("output: {stdout}");
+    println!("That took {:?}", start.elapsed());
     Ok(())
 }
